da0cb53d77e07492cf18708063e8e3aebd5decf4
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-04-06 11:54:42 +0330

    created customers model

---
+++ models/Customer.js
@@ -0,0 +1,37 @@
+import { Schema, model, models } from "mongoose";
+
+const customerSchema = Schema({
+  name: {
+    type: String,
+    require: true, //حتما وارد شود
+    minLength: 1,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    minLength: 1,
+  },
+  email: {
+    type: String,
+    require: true,
+    minLength: 1,
+  },
+  phone: String,
+  address: String,
+  postalCode: Number,
+  data: Date,
+  products: {
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true, //قابل تغییر نباشد
+  },
+  updatedAt: { type: Date, default: () => Date.now() },
+});
+
+const Customer = models.Customer || model("Customer", customerSchema);
+
+export default Customer;
